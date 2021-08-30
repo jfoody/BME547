0af2397d87f5b616158035a3fb79b7bb468f2bba
jfoody <jnfoody@gmail.com>
2021-08-30 13:04:59 -0400

    created working interface for blood level calculation

---
+++ blood_calculator.py
@@ -0,0 +1,13 @@
+def interface():
+	print('Blood calculator')
+	print('make a choice')
+	print('9 - quit')
+	choice 
+
+
+def HDL_driver():
+	hdl_input()
+	hdl_analysis()
+	hdl_output()
+
+def HDL_input():
