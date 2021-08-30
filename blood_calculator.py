9d0d31e55aadb569a8cb43cb551613ea074b2961
jfoody <jnfoody@gmail.com>
2021-08-30 15:27:57 -0400

    added while loop to allow for continuous running

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,12 +1,19 @@
 def interface():
 	print('Blood calculator')
-	print('make a choice')
-	print('9 - quit')
-	choice = int(input('Enter your choice: '))
-	print('your choice is:')
+	keeprunning = True
+	while keeprunning:
+		print('make a choice')
+		print('1-HDL analysis')
+		print('9 - quit')
+		choice = int(input('Enter your choice: '))
 
-	if choice==9:
-		return
+		if choice==9:
+			keeprunning = False
+		elif choice==1:
+			HDL_driver()
+
+	print(choice)
+	return choice
 
 interface()
 
