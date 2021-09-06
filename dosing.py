68313e58e51a57f72c9a3baec03b12f26a396cba
jfoody <jnfoody@gmail.com>
2021-09-06 12:43:16 -0400

    rearranged the code to be in modular style

--- dosing.py
+++ dosing.py
@@ -6,6 +6,13 @@
 """
 
 def dose_amount():
+    diagnosis = get_user_input_diagnosis()
+    weight_in = get_user_input_patient_weight()
+    weight = convert_weight_to_kg(weight_in)
+    dosage_mg_first_day = calculate_dosage(diagnosis, weight)
+    output_results(weight, dosage_mg_first_day)
+
+def get_user_input_diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -14,18 +21,30 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def get_user_input_patient_weight():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input
+
+def convert_weight_to_kg(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def calculate_dosage(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def output_results(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
