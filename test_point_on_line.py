ce361811fe3e7b4ded4365c46f8eb9a4374a4143
jfoody <jnfoody@gmail.com>
2021-09-13 17:48:34 -0400

    added () to the input of the decorator so that it was formatted correctly

--- test_point_on_line.py
+++ test_point_on_line.py
@@ -2,8 +2,8 @@ import pytest
 
 
 # test functionality of point_on_line.py
-@pytest.mark.parametrize('point_one, point_two, new_x, expected', [
-    [0, 0], [10,10], 3, 3])
+@pytest.mark.parametrize('point_one, point_two, new_x, expected',[
+    ([0, 0], [10,10], 3, 3.0)])
 def test_point_on_line(point_one, point_two, new_x, expected):
     from point_on_line import find_y_coord
     answer = find_y_coord(point_one, point_two, new_x)
